8bb81e8cc2c906b5eea8e80d993d2f7a3bbe2624
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-07-18 22:10:06 +0900

    Fixed freezing on window change

--- Assets/Scripts/AI/AICollision.cs
+++ Assets/Scripts/AI/AICollision.cs
@@ -13,6 +13,7 @@ public class AICollision : MonoBehaviour
             if(enemy.health.health <= 0)
             {
                 Destroy(gameObject);
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Lobby");
             }
         }
     }
