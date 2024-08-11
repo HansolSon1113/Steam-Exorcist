15ad52b53277fe5bc3c5722936450335b8a4396d
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-08-11 22:57:34 +0900

    Jump Update

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -51,7 +51,7 @@ public class MovementController : MonoBehaviour, CameraController
 
         if (PlayerController.player.isFlying && !highJump)
         {
-            player.AddForce(Vector3.down * gravityAmount);
+            player.AddForce(Vector3.down * gravityAmount * Time.deltaTime);
         }
         else if (!PlayerController.player.isFlying && v == 1)
         {
