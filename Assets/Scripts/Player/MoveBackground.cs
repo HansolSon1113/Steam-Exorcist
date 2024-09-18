490fc65869537a8140c9545deaccf37599a8fefe
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-09-18 23:57:41 +0900

    Stage1 Background

---
+++ Assets/Scripts/Player/MoveBackground.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private Vector3 orgPlayer = new Vector3(0, 0, 0);
+
+    void Update()
+    {
+        this.transform.position += (orgPlayer - PlayerController.movementController.playerTransform.position) * 0.5f;
+        orgPlayer = PlayerController.movementController.playerTransform.position;
+    }
+}
